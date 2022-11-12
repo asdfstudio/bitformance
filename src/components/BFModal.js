383ab01c96adf5523b03aed612e10df43939c2a7
Will Gu <willgu29@gmail.com>
2022-11-12 14:23:05 -0500

    sign up modal

--- src/components/BFModal.js
+++ src/components/BFModal.js
@@ -1,12 +1,14 @@
 import ContactUs from './modals/ContactUsModal'
+import SignIn from './modals/SignInModal'
+import SignUp from './modals/SignUpModal'
 
 export default function BFModal({ showModalType, setShowModalType }) {
 
 
 	const modals = [
 		{ type: 'CONTACT_US' , component: <ContactUs setShowModalType={setShowModalType} /> },
-		{ type: 'SIGN_UP', component: </> },
-		{ type: 'SIGN_IN', component: </> }
+		{ type: 'SIGN_UP', component: <SignUp /> },
+		{ type: 'SIGN_IN', component: <SignIn /> }
 	]
 
 	const modal = modals.find(obj => obj.type === showModalType)
@@ -14,7 +16,7 @@ export default function BFModal({ showModalType, setShowModalType }) {
 	return(
 		<div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 		  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-		  <div onClick={() => setShowModalType('')} class="fixed inset-0 z-10 overflow-y-auto">
+		  <div class="fixed inset-0 z-10 overflow-y-auto">
 		    {modal.component}
 		  </div>
 		</div>
--- src/components/modals/SignInModal.js
+++ src/components/modals/SignInModal.js
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom'
+import bitLogoWhite from '../../bitLogoWhite.png'
+
 export default function SignInModal({ setShowModalType }) {
 	
 	const signIn = () => {
@@ -6,26 +9,44 @@ export default function SignInModal({ setShowModalType }) {
 
 	return(
 		<div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-		  <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-		    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 bg-gray-50">
-		      <div className="sm:flex items-center">
-		        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
-		          <BFIcon color="#40C8B8" iconName="contact-us" size="2xl" />
-		        </div>
-		        <div className="text-center sm:mt-0 sm:ml-4 sm:text-left">
-		          <h3 className="text-xl font-medium leading-6 text-gray-900" id="modal-title">Contact Us</h3>
-		        </div>
-		      </div>
-		    </div>
+		 
+		  <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
+	
+	   		<button 
+	   			onClick={() => setShowModalType('')} 
+	   			className="absolute top-0 right-4 rounded-full bg-gray-100"
+	   		>
+	   			x
+	   		</button>
 
-		    <div className="px-4">
-		    	TODO: layout for Sign in
-		    </div>
+		    <div className="flex flex-row">
+		    	<div className="bg-blue-800 p-4 text-white space-y-8 flex flex-col">
+		    		<img src={bitLogoWhite} alt="bitformance logo" />
+		    		<h1 className="text-4xl">Sign in to Bitformance</h1>
+		    		<p>Bitformance is the easiest <br/> place to track, analyze, and create cryptocurrency indexes.</p>
+		    		<div className="mt-auto space-y-2">
+		    			<p>Don&apos;t have an account?</p>
+		    			<button className="w-full text-sm rounded bg-blue-100 font-bold text-blue-500 py-2" onClick={() => setShowModalType('SIGN_UP')}>Sign Up</button>
+		    		</div>
+		    	</div>
 
-		    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-		      <button onClick={() => signIn()} type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Submit</button>
-		      <button onClick={() => setShowModalType('')} type="button" className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+		    	<div>
+		    		<label>Username or Email Address</label>
+		    		<input type="text" />
+
+		    		<label>Password</label>
+		    		<input type="password" />
+
+		    		<input type="checkbox" />
+		    		<label>Remember</label>
+
+		    		<Link>Forgot Password?</Link>
+
+		    		{/* RECAPTCHA */}
+		    		<button onClick={() => signIn()}>Sign In</button>
+		    	</div>
 		    </div>
 		  </div>
 		</div>
+	)
 }
