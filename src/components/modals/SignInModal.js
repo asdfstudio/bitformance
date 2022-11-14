26fb39e159c28f032e9886817bf20bc56760d620
Will Gu <willgu29@gmail.com>
2022-11-14 09:29:23 -0500

    sign in form

--- src/components/modals/SignInModal.js
+++ src/components/modals/SignInModal.js
@@ -5,6 +5,7 @@ import SignInForm from '../small/SignInForm'
 export default function SignInModal({ setShowModalType }) {
 
 	const [forgotPassword, setForgotPassword] = useState(false);
+	const [viewShown, setViewShown] = useState('sign-in')
 
 	return(
 		<div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
@@ -13,11 +14,11 @@ export default function SignInModal({ setShowModalType }) {
 			
 		    <div className="grid grid-cols-1 sm:grid-cols-5">
 		    	<div className="col-span-2 bg-blue-900 p-4 text-white flex flex-col space-y-14">
-		    		<BitformanceLeftPanel setShowModalType={setShowModalType} showInfo={!forgotPassword}  />
+		    		<BitformanceLeftPanel setShowModalType={setShowModalType} showInfo={viewShown === 'sign-in'}  />
 		    	</div>
 
 		    	<div className="col-span-3 p-6 space-y-4">
-		    		<SignInForm setShowModalType={setShowModalType} forgotPassword={forgotPassword} setForgotPassword={setForgotPassword} />
+		    		<SignInForm setShowModalType={setShowModalType} setViewShown={setViewShown} viewShown={viewShown} />
 		    	</div>
 		    </div>
 		  </div>
--- src/components/small/SignInForm.js
+++ src/components/small/SignInForm.js
@@ -2,7 +2,7 @@ import ReCAPTCHA from "react-google-recaptcha"
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
-export default function SignInForm({ setShowModalType, forgotPassword, setForgotPassword }) {
+export default function SignInForm({ setShowModalType, viewShown, setViewShown }) {
 
 	const [completedRecaptcha, setCompletedRecaptcha] = useState(false)
 	const [isRemember, setIsRemember] = useState(false);
@@ -11,14 +11,34 @@ export default function SignInForm({ setShowModalType, forgotPassword, setForgot
 		//TODO: sign in
 	}
 
+	const backToSignIn = () => {
+		setViewShown('sign-in')
+	}
+
 	const resetPasswordLink = () => {
 		//TODO:
+		setViewShown('reset-link')
 	}
 
 	function onRecaptchaChange(value) {
 	  console.log('captch value: ', value)
 	  setCompletedRecaptcha(true)
 	}
+
+	const ControlButtons = ({ backTo = 'sign-in' }) => (<>
+		<button 
+			onClick={() => setViewShown(backTo)} 
+			className="float-left rounded-full px-1 bg-gray-100"
+		>
+			back
+		</button>
+ 		<button 
+ 			onClick={() => setShowModalType('')} 
+ 			className="absolute top-0 right-4 rounded-full px-1 bg-gray-100"
+ 		>
+ 			x
+ 		</button>
+	</>)
 
 	const SignIn = () => (<>
 		<button 
@@ -41,7 +61,7 @@ export default function SignInForm({ setShowModalType, forgotPassword, setForgot
 			<input type="checkbox" />
 			<label>Remember</label>
 
-			<button onClick={() => setForgotPassword(true)} className="ml-auto underline text-blue-500">Forgot Password?</button>
+			<button onClick={() => setViewShown('forgot-password')} className="ml-auto underline text-blue-500">Forgot Password?</button>
 		</div>
 
 		<ReCAPTCHA
@@ -56,18 +76,7 @@ export default function SignInForm({ setShowModalType, forgotPassword, setForgot
 	</>)
 
 	const ForgotPassword = () => (<>
-			<button 
-				onClick={() => setForgotPassword(false)} 
-				className="float-left rounded-full px-1 bg-gray-100"
-			>
-				back
-			</button>
-   		<button 
-   			onClick={() => setShowModalType('')} 
-   			className="absolute top-0 right-4 rounded-full px-1 bg-gray-100"
-   		>
-   			x
-   		</button>
+			<ControlButtons backTo="sign-in" />
    		<br />
 
    		<h1 className="text-2xl">Forgot your password?</h1>
@@ -83,5 +92,18 @@ export default function SignInForm({ setShowModalType, forgotPassword, setForgot
    		</div>
 	</>)
 
-	return forgotPassword ? <ForgotPassword /> : <SignIn />
+	const ResetLinkSent =() => (<>
+		<ControlButtons backTo="forgot-password" />
+		<br />
+
+		<h1 className="text-2xl">Check your email</h1>
+		<p>If this email address exists in our system, you will receive an email containing instructions on how to reset your password. Please check your spam if you can not find the email.</p>
+		<div className="mt-auto">
+			<button className="font-bold w-full text-sm rounded bg-blue-500 hover:bg-blue:600 text-white py-2" onClick={() => backToSignIn()}>Go to Sign In</button>
+		</div>
+	</>)
+
+	return viewShown === 'reset-link' ? <ResetLinkSent /> 
+	: viewShown === 'forgot-password' ? <ForgotPassword /> 
+	: <SignIn />
 }
