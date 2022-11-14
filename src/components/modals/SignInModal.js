4ff49bc8b1c4dc62743bd2ad1c3bd2ce20464264
Will Gu <willgu29@gmail.com>
2022-11-14 08:47:46 -0500

    sign in form

--- src/components/modals/SignInModal.js
+++ src/components/modals/SignInModal.js
@@ -1,10 +1,20 @@
 import { Link } from 'react-router-dom'
 import bitLogoWhite from '../../bitLogoWhite.png'
+import ReCAPTCHA from "react-google-recaptcha"
+import { useState } from 'react'
 
 export default function SignInModal({ setShowModalType }) {
 	
+	const [completedRecaptcha, setCompletedRecaptcha] = useState(false)
+	const [isRemember, setIsRemember] = useState(false);
+
 	const signIn = () => {
 		//TODO: sign in
+	}
+
+	function onRecaptchaChange(value) {
+	  console.log('captch value: ', value)
+	  setCompletedRecaptcha(true)
 	}
 
 	return(
@@ -14,36 +24,50 @@ export default function SignInModal({ setShowModalType }) {
 	
 	   		<button 
 	   			onClick={() => setShowModalType('')} 
-	   			className="absolute top-0 right-4 rounded-full bg-gray-100"
+	   			className="absolute top-0 right-4 rounded-full px-1 bg-gray-100"
 	   		>
 	   			x
 	   		</button>
 
-		    <div className="flex flex-row">
-		    	<div className="bg-blue-800 p-4 text-white space-y-8 flex flex-col">
-		    		<img src={bitLogoWhite} alt="bitformance logo" />
-		    		<h1 className="text-4xl">Sign in to Bitformance</h1>
-		    		<p>Bitformance is the easiest <br/> place to track, analyze, and create cryptocurrency indexes.</p>
+		    <div className="grid grid-cols-1 sm:grid-cols-5">
+		    	<div className="col-span-2 bg-blue-900 p-4 text-white flex flex-col space-y-16">
+		    		<div className="space-y-4">
+		    			<img className="w-44" src={bitLogoWhite} alt="bitformance logo" />
+		    			<h1 className="text-4xl mt-2">Sign in to Bitformance</h1>
+		    			<p>Bitformance is the easiest <br/> place to track, analyze, and create cryptocurrency indexes.</p>
+		    		</div>
 		    		<div className="mt-auto space-y-2">
 		    			<p>Don&apos;t have an account?</p>
-		    			<button className="w-full text-sm rounded bg-blue-100 font-bold text-blue-500 py-2" onClick={() => setShowModalType('SIGN_UP')}>Sign Up</button>
+		    			<button className="w-full text-sm rounded bg-blue-100 hover:bg-blue-200 font-bold text-blue-500 py-2" onClick={() => setShowModalType('SIGN_UP')}>Sign Up</button>
 		    		</div>
 		    	</div>
 
-		    	<div>
-		    		<label>Username or Email Address</label>
-		    		<input type="text" />
+		    	<div className="col-span-3 p-6 space-y-4">
 
-		    		<label>Password</label>
-		    		<input type="password" />
+		    		<div>
+		    			<label className="font-bold">Username or Email Address</label>
+		    			<input className="w-full py-1 border rounded" type="text" />
+		    		</div>
 
-		    		<input type="checkbox" />
-		    		<label>Remember</label>
+		    		<div>
+		    			<label className="font-bold">Password</label>
+		    			<input className="w-full py-1 border rounded" type="password" />
+		    		</div>
 
-		    		<Link>Forgot Password?</Link>
+		    		<div className="flex flex-row gap-2">
+		    			<input type="checkbox" />
+		    			<label>Remember</label>
 
-		    		{/* RECAPTCHA */}
-		    		<button onClick={() => signIn()}>Sign In</button>
+		    			<Link className="ml-auto underline text-blue-500">Forgot Password?</Link>
+		    		</div>
+
+		    		<ReCAPTCHA
+		    		  sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
+		    		  onChange={onRecaptchaChange}
+		    		  className="ml-0 sm:ml-6"
+		    		/>
+
+		    		<button className="mt-auto w-full text-sm rounded bg-blue-500 hover:bg-blue:600 text-white py-2" onClick={() => signIn()}>Sign In</button>
 		    	</div>
 		    </div>
 		  </div>
