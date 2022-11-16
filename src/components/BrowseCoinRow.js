ca36ed000835a5c379bfc44181751345a506a580
Will Gu <willgu29@gmail.com>
2022-11-16 17:02:00 -0500

    browse

---
+++ src/components/BrowseCoinRow.js
@@ -0,0 +1,54 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+export default function BrowseCoinRow({
+	id,
+	image,
+	name,
+	ticker,
+	price,
+	hourlyPercentageChange,
+	weeklyPercentageChange,
+	marketCap,
+}) {
+
+	const DownTag = ({ change }) => (<div className="w-20 mx-auto">
+			<div className="flex flex-row px-2 py-1 rounded-lg justify-center items-center gap-1 rounded">
+				<FontAwesomeIcon icon={faArrowLeft} size="xs" transform={{ rotate: -45}} color="red" />
+				<p className="text-sm">{Math.abs(change).toFixed(2)}%</p>
+			</div>
+		</div>
+	)
+
+	const UpTag = ({ change }) => (<div className="w-20 mx-auto">
+			<div className="flex flex-row px-2 py-1 rounded-lg justify-center items-center gap-1 rounded">
+				<FontAwesomeIcon icon={faArrowRight} size="xs" transform={{ rotate: -45}} color="green" />
+				<p className="text-sm">{Math.abs(change).toFixed(2)}%</p>
+			</div>
+		</div>
+	)
+
+	return(<>
+		<td className="flex flex-row items-center gap-2">
+			<img className="w-6 h-6" src={image} alt={name} />
+			<p>{name}</p>
+		</td>
+		<td className="text-sm">${price}</td>
+		<td>
+			{hourlyPercentageChange > 0 
+				? <UpTag change={hourlyPercentageChange} />
+				: <DownTag change={hourlyPercentageChange} />
+			}
+		</td>
+		<td>	
+			{weeklyPercentageChange > 0 
+				? <UpTag change={weeklyPercentageChange} />
+				: <DownTag change={weeklyPercentageChange} />
+			}
+		</td>
+		<td className="text-sm pl-5">{marketCap}</td>
+		<td>date time tag, weighting balancing</td>
+		<td>Actions</td>
+
+	</>)
+}	
--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import BrowseCoinRow from '../components/BrowseCoinRow'
+import exampleIcon from '../exampleIcon.png'
 
 export default function BrowsePage() {
 
@@ -19,8 +21,38 @@ export default function BrowsePage() {
 	const [sortOrder, setSortOrder] = useState('ASC')
 
 	const data = [
-
+		{
+			id: 109,
+			image: exampleIcon,
+			name: 'Bitcoin',
+			ticker: 'BTC',
+			price: 20928.35,
+			hourlyPercentageChange: -0.91,
+			weeklyPercentageChange: -2.11,
+			marketCap: '$380.44B'
+		},
+		{
+			id: 209,
+			image: exampleIcon,
+			name: 'Ethereum',
+			ticker: 'ETH',
+			price: 928.35,
+			hourlyPercentageChange: 1.31,
+			weeklyPercentageChange: 21.1,
+			marketCap: '$320.44B'
+		},
+		{
+			id: 309,
+			image: exampleIcon,
+			name: 'Tether',
+			ticker: 'USDT',
+			price: 28.35,
+			hourlyPercentageChange: 12.04,
+			weeklyPercentageChange: -62.09,
+			marketCap: '$10.44M'
+		},
 	]
+
 
 	return (
 		<div className="p-4">
@@ -38,7 +70,9 @@ export default function BrowsePage() {
 					</tr>
 				</thead>
 				<tbody>
-				
+					{data.map(coin => (
+						<tr key={coin.id}><BrowseCoinRow {...coin} /></tr>
+					))}
 				</tbody>
 			</table>
 		</div>
