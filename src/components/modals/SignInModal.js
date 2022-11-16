44aa36dc71c92dda071e6abd87e273f97af74257
Will Gu <willgu29@gmail.com>
2022-11-16 15:25:04 -0500

    layout fix

--- src/components/modals/SignInModal.js
+++ src/components/modals/SignInModal.js
@@ -13,11 +13,11 @@ export default function SignInModal({ setShowModalType }) {
 		  <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
 			
 		    <div className="grid grid-cols-1 sm:grid-cols-5">
-		    	<div className="col-span-2 bg-blue-900 p-4 text-white flex flex-col space-y-14">
+		    	<div className="col-span-2 bg-blue-900 text-white flex flex-col p-4 space-y-15">
 		    		<BitformanceLeftPanel setShowModalType={setShowModalType} showInfo={viewShown === 'sign-in'}  />
 		    	</div>
 
-		    	<div className="col-span-3 p-6 space-y-4">
+		    	<div className="col-span-3 p-4 space-y-4">
 		    		<SignInForm setShowModalType={setShowModalType} setViewShown={setViewShown} viewShown={viewShown} />
 		    	</div>
 		    </div>
