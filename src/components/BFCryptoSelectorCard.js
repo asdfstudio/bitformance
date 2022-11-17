9af4724734d41936a8bbf842ffa0512b20873fde
Will Gu <willgu29@gmail.com>
2022-11-17 01:23:59 -0500

    wired

--- src/components/BFCryptoSelectorCard.js
+++ src/components/BFCryptoSelectorCard.js
@@ -1,16 +1,33 @@
 import BFIcon from './BFIcon'
 
-export default function BFCryptoSelectorCard() {
+export default function BFCryptoSelectorCard({ selectedCrypto, selectCrypto, id }) {
+	
+	//TODO: show cryptos list from DB 
+	const showOptions = (text) => {
+		selectCrypto(text, id)
+	}
+
+	const DefaultView = () => (
+		<div className="text-center my-auto">
+			<BFIcon iconName="scale" size="8x" color="#ECECEC" />
+		</div>
+	)
+
+	const CryptoView = () => (
+		<div>
+			{JSON.stringify(selectedCrypto)}
+		</div>
+	)
+
 	return(
 		<div className="bg-white shadow rounded-md p-4 flex flex-col">
 			<h2 className="text-lg">Choose Cryptocurrency</h2>
-			<div className="mt-2 flex flex-row items-center gap-2 border rounded p-1 bg-gray-50">
+			<div className="mt-2 flex flex-row items-center gap-2 border rounded px-2 py-1 bg-gray-50">
 				<BFIcon iconName="search" color="gray" /> 
-				<input className="w-full !outline-none bg-gray-50" type="text" placeholder="Search indexes to compare.." />
+				<input onChange={(e) => showOptions(e.target?.value)} className="w-full !outline-none bg-gray-50" type="text" placeholder="Search indexes to compare.." />
 			</div>
-			<div className="text-center my-auto">
-				<BFIcon iconName="scale" size="8x" color="#ECECEC" />
-			</div>
+
+			{selectedCrypto ? <CryptoView /> : <DefaultView />}
 		</div>
 	)
 }
--- src/pages/ComparePage.js
+++ src/pages/ComparePage.js
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BFIcon from '../components/BFIcon'
 import BFCryptoSelectorCard from '../components/BFCryptoSelectorCard'
+import exampleIcon from '../exampleIcon.png'
 
 export default function ComparePage() {
 
 	const [typeSelected, setTypeSelected] = useState('side-by-side')
+
+	const [selectedCryptoOne, setSelectedCryptoOne] = useState(null)
+	const [selectedCryptoTwo, setSelectedCryptoTwo] = useState(null)
+
 
 	const selectedStyle = 'border shadow rounded-md bg-blue-400 text-white p-2 cursor'
 	const notSelectedStyle = 'rounded-md bg-gray-100 hover:bg-gray-200 text-gray-500 p-2 cursor'
@@ -23,17 +28,34 @@ export default function ComparePage() {
 		</button>
 	)
 
+	const selectCrypto = (text, id) => {
+		//TODO: select crypto from text and 
+		console.log(text)
+
+		const setCall = id == 1 ? setSelectedCryptoOne : setSelectedCryptoTwo
+		setCall({
+			id: 109,
+			image: exampleIcon,
+			name: 'Bitcoin',
+			ticker: 'BTC',
+			price: 20928.35,
+			hourlyPercentageChange: -0.91,
+			weeklyPercentageChange: -2.11,
+			marketCap: '$380.44B'
+		})
+	}
+
 	return(
-		<div className="p-4 h-4/5">
+		<div className="p-4 h-5/6">
 			<div className="flex flex-row gap-4 rounded-lg border bg-white p-4 shadow items-center">
 				<h1 className="text-xl mr-2">Compare Currencies</h1>
 				{typeSelected === 'side-by-side' ? <SelectedButton label="Side-by-side" /> : <NotSelectedButton label="Side-by-side" />}
 				{typeSelected === 'overlay' ? <SelectedButton label="Overlay" /> : <NotSelectedButton label="Overlay" />}
 			</div>
 
-			<div className="grid grid-cols-2 gap-2 p-1 h-4/5">
-				<BFCryptoSelectorCard />
-				<BFCryptoSelectorCard />
+			<div className="grid grid-cols-2 gap-2 p-1 h-4/5 overflow-y-auto">
+				<BFCryptoSelectorCard selectedCrypto={selectedCryptoOne} selectCrypto={selectCrypto} id="1" />
+				<BFCryptoSelectorCard selectedCrypto={selectedCryptoTwo} selectCrypto={selectCrypto} id="2" />
 			</div>
 		</div>
 	)
