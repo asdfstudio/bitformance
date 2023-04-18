4423a7b8c5cc24839e3995ce016770d7f9ab3d6d
TIRony <rony120114@gmail.com>
2023-04-18 11:59:51 +0600

    Twitter embeb

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,13 +10,13 @@ export default function HomePage() {
 
 	return(
 		<div className="p-0 bg-main-lightGray space-y-4 relative sm:p-4">
-			<div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+			<div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 pt-2">
 				<RecentlyAdded />
 				<PerformersCard />
 				<MostPopularCard />
 			</div>
 			<GraphCard title="Top 50 Currency Indexes" hook={useTopFifty} />
-			<div className="grid grid-cols-3 gap-6 xl:grid-cols-4">
+			<div className="grid grid-cols-3 gap-4 xl:grid-cols-4">
 				<div className="bg-main-white col-start-1 col-span-3 rounded-2xl border border-main-inputBorder">
 					<TopMarketCap />
 				</div>
