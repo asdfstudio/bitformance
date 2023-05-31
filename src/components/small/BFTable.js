7004c1dd41171bcaa7a19532f0dfc634bb26a578
unknown <rony120114@gmail.com>
2023-05-31 11:29:56 +0600

    safari browser error

--- src/components/small/BFTable.js
+++ src/components/small/BFTable.js
@@ -137,7 +137,7 @@ export default function BFTable({ showHeader = true, handleHeaderClick = functio
 									<div className='flex flex-row items-center justify-center w-screen -mr-3 -ml-2 sm:w-auto'>
 										{showHoldingRow === index && 
 										<td 
-											className={`bg-main-white rounded-b-xl drop-shadow-lg border-t-[1px] border-main-lightGrayBorder pt-4 cursor-pointer overflow-x-auto max-w-full z-auto touch-auto overflow-y-hidden`} 
+											className={`bg-main-white rounded-b-xl drop-shadow-lg border-t-[1px] border-main-lightGrayBorder pt-4 cursor-pointer overflow-x-scroll max-w-full z-auto touch-auto overflow-y-hidden whitespace-nowrap`} 
 											colspan="8">
 											<BFHoldingsTable holdings={coin.rawStocks.map(stock => {
 												return {
