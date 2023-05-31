36c7b8f620f3d2385bc659d5ff771cfa4f6f8734
unknown <rony120114@gmail.com>
2023-05-31 14:24:38 +0600

    safari browser error

--- src/components/small/BFTable.js
+++ src/components/small/BFTable.js
@@ -132,13 +132,11 @@ export default function BFTable({ showHeader = true, handleHeaderClick = functio
 											/>
 										</td>
 									</div>
-									
-								</tr>
-								<div className='flex flex-row items-center justify-center w-screen -mr-2 -ml-2 sm:w-auto pl-2 pr-2'>
+									<div className='flex flex-row items-center justify-center w-[350px] -mr-2 -ml-2 sm:w-auto pl-2 pr-2'>
 										{showHoldingRow === index && 
 										<td 
 											className={`bg-main-white rounded-b-xl drop-shadow-lg border-t-[1px]
-												border-main-lightGrayBorder pt-4 cursor-pointer overflow-x-auto max-w-full
+												border-main-lightGrayBorder pt-4 cursor-pointer overflow-x-scroll max-w-full
 											`} 
 											colspan="8">
 											<BFHoldingsTable holdings={coin.rawStocks.map(stock => {
@@ -151,6 +149,7 @@ export default function BFTable({ showHeader = true, handleHeaderClick = functio
 										</td>
 										}
 									</div>
+								</tr>
 							</React.Fragment>)
 							: (<tr key={coin.id}>no valid type</tr>)
 					})}
